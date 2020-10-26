92b015459e1d78a64129365c546f2ab95d8a855a
Alessandro <alessandro.boscato@gmail.com>
2020-10-26 16:33:18 +0100

    Step 8/9
    Creazione route che manda al controller, il quale a sua volta restituisce la view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student; //MODEL
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+      return view('home');
+    }
 }
